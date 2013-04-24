f89f7ee75df44a4409afa6196c418e7ae7d5d4a7
JekaMelnikov <jeka.melnikov@gmail.com>
2013-04-25 01:12:47 +0400

    Add function for debug version.

--- defs.h
+++ defs.h
@@ -1,2 +1,37 @@
+#include <math.h>
+
 #define VISC    0.01
 #define PP      10
+#ifndef PI
+#define PI 3.141592653589793
+#endif
+
+double debug_rho (double t, double x, double y)
+{
+  return exp (t) * (cos (2 * PI * x) * cos (2 * PI * y + PI / 4) + 1.5);
+}
+
+double debug_v1 (double t, double x, double y)
+{
+  return exp (x - y) * cos (2 * PI * t) * sin (25 * PI * x * x) * sin (32 * PI * y * y);
+}
+
+double debug_v2 (double t, double x, double y)
+{
+  return exp (y - x) * sin (2 * PI * t) * sin (50 * PI * x * x) * sin (16 * PI * y * y);
+}
+
+double init_rho (double x, double y)
+{
+  return debug_rho (0, x, y);
+}
+
+double init_v1 (double x, double y)
+{
+  return debug_v1 (0, x, y);
+}
+
+double init_v2 (double x, double y)
+{
+  return debug_v2 (0, x, y);
+}
